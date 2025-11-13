3a99935be307fda268e007b037dcbb612ee3df2d
karannchaudhari04 <karanchaudhari34804@gmail.com>
2025-11-13 11:52:54 +0530

    created frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState(0)
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/message')
+      .then((res) => res.json())
+      .then((data) => {setMessage(data.message)})
+  }, [])
+
+  return (
+    <>
+      <h1>This is simple frontend</h1>
+      <h2>data {message}</h2>
+    </>
+  )
+}
+
+export default App

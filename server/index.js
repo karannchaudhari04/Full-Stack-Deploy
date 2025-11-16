07cb535bb1e95cbd9994463ff9fc6482a6dc868e
karannchaudhari04 <karanchaudhari34804@gmail.com>
2025-11-16 13:37:47 +0530

    corrected index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(cors(
   {
     origin:['http://localhost:3000',
             'http://localhost:5173',
-            'https://localhost:5174',
+            'http://localhost:5174',
             //'https://my-production-domain.com'
     ],
     credentials:true,

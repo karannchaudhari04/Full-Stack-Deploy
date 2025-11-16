183c7e431693a18a1f5c16246615668aceb9f422
karannchaudhari04 <karanchaudhari34804@gmail.com>
2025-11-16 12:22:46 +0530

    updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     fetch('http://localhost:4000/api/message')
       .then((res) => res.json())
       .then((data) => {setMessage(data.message)})
+      .catch((err) => console.error('Error fetching message:', err))
   }, [])
 
   return (
